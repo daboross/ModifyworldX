8c6f87e187be979a1d4c8278978687c923bb0648
Dabo Ross <daboross@daboross.net>
2013-07-09 23:51:08 -0700

    Revert "Changed message format"
    This reverts commit 00a07c330e0334c225c8c776dd671c9ddc53738a.

--- src/main/java/ru/tehkode/modifyworld/bukkit/Modifyworld.java
+++ src/main/java/ru/tehkode/modifyworld/bukkit/Modifyworld.java
@@ -37,7 +37,6 @@ import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 /**
  *
@@ -45,120 +44,120 @@ import java.util.logging.Level;
  */
 public class Modifyworld extends JavaPlugin {
 
-    protected final static Class<? extends ModifyworldListener>[] LISTENERS = new Class[]{
-        PlayerListener.class,
-        EntityListener.class,
-        BlockListener.class,
-        VehicleListener.class
-    };
-    protected List<ModifyworldListener> listeners = new ArrayList<ModifyworldListener>();
-    protected PlayerInformer informer;
-    protected File configFile;
-    protected FileConfiguration config;
+	protected final static Class<? extends ModifyworldListener>[] LISTENERS = new Class[]{
+		PlayerListener.class,
+		EntityListener.class,
+		BlockListener.class,
+		VehicleListener.class
+	};
+	protected List<ModifyworldListener> listeners = new ArrayList<ModifyworldListener>();
+	protected PlayerInformer informer;
+	protected File configFile;
+	protected FileConfiguration config;
 
-    @Override
-    public void onLoad() {
-        configFile = new File(this.getDataFolder(), "config.yml");
-    }
+	@Override
+	public void onLoad() {
+		configFile = new File(this.getDataFolder(), "config.yml");
+	}
 
-    @Override
-    public void onEnable() {
-        this.config = this.getConfig();
+	@Override
+	public void onEnable() {
+		this.config = this.getConfig();
 
-        if (!config.isConfigurationSection("messages")) {
-            this.getLogger().severe("Deploying default config");
-            this.initializeConfiguration(config);
-        }
+		if (!config.isConfigurationSection("messages")) {
+			this.getLogger().severe("Deploying default config");
+			this.initializeConfiguration(config);
+		}
 
-        this.informer = new PlayerInformer(config);
+		this.informer = new PlayerInformer(config);
 
-        this.registerListeners();
-        this.getLogger().info("Modifyworld enabled!");
+		this.registerListeners();
+		this.getLogger().info("Modifyworld enabled!");
 
-        this.saveConfig();
-    }
+		this.saveConfig();
+	}
 
-    @Override
-    public void onDisable() {
-        this.listeners.clear();
-        this.config = null;
+	@Override
+	public void onDisable() {
+		this.listeners.clear();
+		this.config = null;
 
-        this.getLogger().info("Modifyworld successfully disabled!");
-    }
+		this.getLogger().info("Modifyworld successfully disabled!");
+	}
 
-    protected void initializeConfiguration(FileConfiguration config) {
-        // Flags
-        config.set("item-restrictions", false);
-        config.set("inform-players", false);
-        config.set("whitelist", false);
-        config.set("use-material-names", true);
-        config.set("drop-restricted-item", false);
-        config.set("item-use-check", false);
+	protected void initializeConfiguration(FileConfiguration config) {
+		// Flags
+		config.set("item-restrictions", false);
+		config.set("inform-players", false);
+		config.set("whitelist", false);
+		config.set("use-material-names", true);
+		config.set("drop-restricted-item", false);
+		config.set("item-use-check", false);
 
-        // Messages
-        config.set("messages/message-format", PlayerInformer.DEFAULT_MESSAGE_FORMAT);
-        config.set("messages/default-message", PlayerInformer.PERMISSION_DENIED);
+		// Messages
+		config.set("messages/message-format", PlayerInformer.DEFAULT_MESSAGE_FORMAT);
+		config.set("messages/default-message", PlayerInformer.PERMISSION_DENIED);
 
-        // Predefined messages
-        config.set("messages/modifyworld.login", PlayerInformer.WHITELIST_MESSAGE);
-        config.set("messages/modifyworld.items.have", PlayerInformer.PROHIBITED_ITEM);
-    }
+		// Predefined messages
+		config.set("messages/modifyworld.login", PlayerInformer.WHITELIST_MESSAGE);
+		config.set("messages/modifyworld.items.have", PlayerInformer.PROHIBITED_ITEM);
+	}
 
-    protected void registerListeners() {
-        for (Class listenerClass : LISTENERS) {
-            try {
-                Constructor constructor = listenerClass.getConstructor(Plugin.class, ConfigurationSection.class, PlayerInformer.class);
-                ModifyworldListener listener = (ModifyworldListener) constructor.newInstance(this, this.getConfig(), this.informer);
-                this.listeners.add(listener);
-            } catch (Throwable e) {
-                this.getLogger().log(Level.WARNING, "Failed to initialize \"{0}\" listener", listenerClass.getName());
-                e.printStackTrace(System.err);
-            }
-        }
-    }
+	protected void registerListeners() {
+		for (Class listenerClass : LISTENERS) {
+			try {
+				Constructor constructor = listenerClass.getConstructor(Plugin.class, ConfigurationSection.class, PlayerInformer.class);
+				ModifyworldListener listener = (ModifyworldListener) constructor.newInstance(this, this.getConfig(), this.informer);
+				this.listeners.add(listener);
+			} catch (Throwable e) {
+				this.getLogger().warning("Failed to initialize \"" + listenerClass.getName() + "\" listener");
+				e.printStackTrace();
+			}
+		}
+	}
 
-    @Override
-    public FileConfiguration getConfig() {
-        if (this.config == null) {
-            this.reloadConfig();
-        }
+	@Override
+	public FileConfiguration getConfig() {
+		if (this.config == null) {
+			this.reloadConfig();
+		}
 
-        return this.config;
-    }
+		return this.config;
+	}
 
-    @Override
-    public void saveConfig() {
-        try {
-            this.config.save(configFile);
-        } catch (IOException e) {
-            this.getLogger().log(Level.SEVERE, "Failed to save configuration file: {0}", e.getMessage());
-        }
-    }
+	@Override
+	public void saveConfig() {
+		try {
+			this.config.save(configFile);
+		} catch (IOException e) {
+			this.getLogger().severe("Failed to save configuration file: " + e.getMessage());
+		}
+	}
 
-    @Override
-    public void reloadConfig() {
-        this.config = new YamlConfiguration();
-        config.options().pathSeparator('/');
+	@Override
+	public void reloadConfig() {
+		this.config = new YamlConfiguration();
+		config.options().pathSeparator('/');
 
-        try {
-            config.load(configFile);
-        } catch (FileNotFoundException e) {
-            this.getLogger().severe("Configuration file not found - deploying default one");
-            InputStream defConfigStream = getResource("config.yml");
-            if (defConfigStream != null) {
-                try {
-                    this.config.load(defConfigStream);
-                } catch (Exception de) {
-                    this.getLogger().severe("Default config file is broken. Please tell this to Modifyworld author.");
-                }
-            }
-        } catch (Exception e) {
-            this.getLogger().log(Level.SEVERE, "Failed to load configuration file: {0}", e.getMessage());
-        }
+		try {
+			config.load(configFile);
+		} catch (FileNotFoundException e) {
+			this.getLogger().severe("Configuration file not found - deploying default one");
+			InputStream defConfigStream = getResource("config.yml");
+			if (defConfigStream != null) {
+				try {
+					this.config.load(defConfigStream);
+				} catch (Exception de) {
+					this.getLogger().severe("Default config file is broken. Please tell this to Modifyworld author.");
+				}
+			}
+		} catch (Exception e) {
+			this.getLogger().severe("Failed to load configuration file: " + e.getMessage());
+		}
 
-        InputStream defConfigStream = getResource("config.yml");
-        if (defConfigStream != null) {
-            this.config.setDefaults(YamlConfiguration.loadConfiguration(defConfigStream));
-        }
-    }
+		InputStream defConfigStream = getResource("config.yml");
+		if (defConfigStream != null) {
+			this.config.setDefaults(YamlConfiguration.loadConfiguration(defConfigStream));
+		}
+	}
 }
